25fb87dfffdfd7e5560fe2911e9b833b51436801
Pranitha Bajaj <pranithabajaj123@gmail.com>
2020-03-17 16:45:36 +0530

    Add project files.

---
+++ WebApplication4/Controllers/SampleController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            List<EMPDATA>L=   DBClass.getEmp();
+
+            return View(L);
+        }
+        public ActionResult ExtractEmp()
+        {
+            int Eno = int.Parse(Request.Form["rdbEmp"].ToString());
+            EMPDATA E = DBClass.getEmpdata(Eno);
+            return View(E);
+        }
+        public ActionResult Index1()
+        {
+            ViewBag.L = DBClass.getEmp();
+
+            return View();
+        }
+        public ActionResult ExtractEmp1()
+        {
+            int Eno = int.Parse(Request.Form["rdbEmp"].ToString());
+            ViewBag.E = DBClass.getEmpdata(Eno);
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            List<EMPDATA> L = DBClass.getEmp();
+
+            return View(L);
+        }
+        public ActionResult ExtractEmp2()
+        {
+            int Eno = int.Parse(Request.QueryString["E"].ToString());
+            EMPDATA E = DBClass.getEmpdata(Eno);
+            return View(E);
+        }
+    }
+}
---
+++ WebApplication4/Models/DBClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class DBClass
+    {
+
+        static DemoEntities D = new DemoEntities();
+        public static List<EMPDATA> getEmp()
+        {
+            var E = from E1 in D.EMPDATAs
+                    select E1;
+            return E.ToList(); 
+        }
+        public static EMPDATA getEmpdata(int eno)
+        {
+            var E = from E1 in D.EMPDATAs
+                    where E1.EMPNO==eno
+                    select E1;
+            return E.First();
+        }
+    }
+}
